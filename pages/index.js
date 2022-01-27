df0135373159b9fdb25449768b93893c69af7526
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-01-27 11:10:24 -0800

    🔧 Modify config files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -13,11 +13,19 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Trigger a sentry error by clicking{" "}
+          <button
+            type="button"
+            onClick={() => {
+              throw new Error("💥 Something went wrong!");
+            }}
+          >
+            here
+          </button>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
---
+++ sentry.server.config.js
@@ -0,0 +1,16 @@
+import * as Sentry from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn:
+    SENTRY_DSN ||
+    "https://c55f1e9c0ca44eb5beffeb5ce41bb24e@leeandher.ngrok.io/8",
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
